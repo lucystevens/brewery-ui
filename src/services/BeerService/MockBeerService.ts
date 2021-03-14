8859b2001104e5cb90e328f9848315ce837d443e
Luke Stevens <lcm.stevens96@gmail.com>
2021-03-14 17:38:24 +0000

    Add images for mock beer data

--- src/services/BeerService/MockBeerService.ts
+++ src/services/BeerService/MockBeerService.ts
@@ -2,15 +2,26 @@ import axios from 'axios';
 import MockAdapter from 'axios-mock-adapter';
 import Beer from '../../models/Beer';
 
-const mockBeers: Beer[] = [{
-    id: 1,
-    slug: "mieterinnen",
-    name: "Mieter*innen",
-    abv: 4.8,
-    logoUrl: "/images/beers/mieterinnen/logo.png",
-    coverImageUrl: "/images/beers/mieterinnen/cover.png",
-    tagLine: "Crisp Helles Lager"
-}]
+const mockBeers: Beer[] = [
+    {
+        id: 1,
+        slug: "seeing-double",
+        name: "Seeing Double",
+        abv: 7.2,
+        logoUrl: "/images/beers/seeing-double/logo.png",
+        coverImageUrl: "/images/beers/seeing-double/cover.jpg",
+        tagLine: "Hazy DDH IPA"
+    },
+    {
+        id: 2,
+        slug: "mieterinnen",
+        name: "Mieter*innen",
+        abv: 4.8,
+        logoUrl: "/images/beers/mieterinnen/logo.png",
+        coverImageUrl: "/images/beers/mieterinnen/cover.jpg",
+        tagLine: "Crisp Helles Lager"
+    }
+]
 
 export const setupBeerServiceMock = () => {
   const mock = new MockAdapter(axios);
