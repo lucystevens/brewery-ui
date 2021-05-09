2290f090e5ba4b91e12ca55ff042abf89c55396a
Luke Stevens <lcm.stevens96@gmail.com>
2021-05-09 22:16:28 +0100

    Make filter more extensible

--- src/components/pages/BeersPage/BeerFilter/BeerFilter.tsx
+++ src/components/pages/BeersPage/BeerFilter/BeerFilter.tsx
@@ -1,6 +1,7 @@
-import React, { ChangeEventHandler, useState } from 'react'
-import { TextField } from '@material-ui/core';
+import React, { ChangeEventHandler, useEffect, useState } from 'react'
+import { TextField, Typography } from '@material-ui/core';
 import Beer from '../../../../models/Beer';
+import { Slider } from '@material-ui/core';
 
 export interface BeerFilterProps {
     beers: Beer[];
@@ -9,15 +10,24 @@ export interface BeerFilterProps {
 
 const BeerFilter: React.FC<BeerFilterProps> = ({ beers, onFilterChange }) => {
 
+  const [searchTerm, setSearchTerm] = useState<string>()
+
     const onSearchTermChanged = (event: any) => {
-      let value = event.target.value.toLowerCase();
-      onFilterChange(beers.filter(
-        b => b.name.toLowerCase().includes(value)))
+      setSearchTerm(event.target.value.toLowerCase())
     }
+
+    useEffect(() => {
+      onFilterChange(beers.filter(
+        b => !searchTerm || b.name.toLowerCase().includes(searchTerm)))
+  }, [searchTerm, beers]);
 
     return (
       <form noValidate>
-        <TextField id="outlined-basic" label="Search" variant="outlined" onChange={onSearchTermChanged}/>
+        <TextField 
+          id="outlined-basic" 
+          label="Search" 
+          variant="outlined" 
+          onChange={onSearchTermChanged}/>
       </form>
     );
 
