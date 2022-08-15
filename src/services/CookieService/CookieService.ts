8aa357642a25b1d4dfe2494e91b2a96b81aaaa35
Lucy Stevens <lcm.stevens96@gmail.com>
2022-08-15 20:59:07 +0100

    Anonymise tracking when users disallow cookies

--- src/services/CookieService/CookieService.ts
+++ src/services/CookieService/CookieService.ts
@@ -1,7 +1,10 @@
+import { reset, setUserId } from "@amplitude/analytics-browser/lib/esm/browser-client";
 import Cookies from "js-cookie"
 import { isDev } from "utils/WebUtils"
 import { v4 as uuidv4 } from 'uuid';
 import CookiePrefs from "./CookiePrefs";
+
+const anonymous = "anonymous"
 
 // Local storage
 const cookiePrefsKey = "cbpCookiePrefs"
@@ -9,6 +12,7 @@ const cookieBannerKey = "cookieBannerDismissed"
 
 export const setCookiePrefs = (prefs: CookiePrefs) => {
     localStorage.setItem(cookiePrefsKey, JSON.stringify(prefs))
+    applyCookiePrefs(prefs)
 }
 
 export const getCookiePrefs = (): CookiePrefs => {
@@ -28,20 +32,56 @@ export const dismissCookieBanner = () => {
 const userIdKey = "cbpUserId"
 const sessionIdKey = "cbpSessionId"
 
+const newUserId = (): string => {
+    return isDev()? "test-user" :
+        getCookiePrefs().analyticsAllowed? uuidv4() :
+        anonymous
+}
+
+const setLocalUserId = (userId: string) => {
+    Cookies.set(userIdKey, userId, { expires: 365 })
+}
+
 export const getUserId = (): string => {
     var userId = Cookies.get(userIdKey)
     if(!userId){
-        userId = isDev()? "test-user" : uuidv4()
-        Cookies.set(userIdKey, userId, { expires: 365 })
+        userId = newUserId()
+        setLocalUserId(userId)
     }
     return userId
+}
+
+
+const newSessionId = (): string => {
+    return getCookiePrefs().analyticsAllowed? uuidv4() : anonymous
 }
 
 export const getSessionId = (): string => {
     var sessionId = Cookies.get(sessionIdKey)
     if(!sessionId){
-        sessionId = uuidv4()
+        sessionId = newSessionId()
         Cookies.set(sessionIdKey, sessionId)
     }
     return sessionId
+}
+
+// cookie utils
+export const applyCookiePrefs = (prefs: CookiePrefs) => {
+    if(!prefs.analyticsAllowed){
+        Cookies.set(userIdKey, anonymous, { expires: 365 })
+        Cookies.set(sessionIdKey, anonymous)
+        reset()
+    }
+    else {
+        var userId = getUserId()
+        if(userId === anonymous){
+            userId = newUserId()
+            setLocalUserId(userId)
+            setUserId(userId)
+        }
+        let sessionId = getSessionId()
+        if(sessionId === anonymous){
+            Cookies.set(sessionIdKey, newSessionId())
+        }
+    }
 }
